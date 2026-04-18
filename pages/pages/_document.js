f525bc88c93092455515209e71b886058cb7c923
ariadnafloo <ariflores92@gmail.com>
2026-04-18 17:13:14 +0200

    Favicon Generator

---
+++ pages/pages/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+      <Html lang="es">
+            <Head>
+                    <link rel="icon" href="/favicon.ico" sizes="32x32" />
+                            <link rel="icon" href="/favicon-512.png" sizes="512x512" type="image/png" />
+                                    <link rel="apple-touch-icon" href="/favicon-192.png" />
+                                            <link rel="manifest" href="/manifest.json" />
+                                                    <meta name="theme-color" content="#002FA7" />
+                                                            <meta name="apple-mobile-web-app-capable" content="yes" />
+                                                                    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+                                                                            <meta name="apple-mobile-web-app-title" content="Mudanza" />
+                                                                                  </Head>
+                                                                                        <body>
+                                                                                                <Main />
+                                                                                                        <NextScript />
+                                                                                                              </body>
+                                                                                                                  </Html>
+                                                                                                                    );
+                                                                                                                    }
